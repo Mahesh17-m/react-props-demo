e213f131b03b3dc2d0cf5b7020983e55f3131a16
Mahesh17-m <mbmandalapu200217@gmail.com>
2025-09-17 13:57:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from "./components/Greeting.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1>Experiment 3: Props Demo</h1>
+      <Greeting name="Srujana" message="Welcome to learning ReactJS props!" />
+      <Greeting name="Student" message="Done Your experiments!" />
+    </div>
+  );
+}
+
+export default App;
